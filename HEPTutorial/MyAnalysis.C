95168ac84744443cb2cee9b599e69f55e89c8fcb
Jiwon Park <minerva1993@gmail.com>
2017-08-18 16:35:55 +0900

    code modi

--- HEPTutorial/MyAnalysis.C
+++ HEPTutorial/MyAnalysis.C
@@ -9,46 +9,46 @@ using namespace std;
 
 void MyAnalysis::BuildEvent() {
 
-	Muons.clear();
-	for (int i = 0; i < NMuon; ++i) {
-	MyMuon muon(Muon_Px[i], Muon_Py[i], Muon_Pz[i], Muon_E[i]);
-	muon.SetIsolation(Muon_Iso[i]);
-	muon.SetCharge(Muon_Charge[i]);
-	Muons.push_back(muon);
-	}
+  Muons.clear();
+  for (int i = 0; i < NMuon; ++i) {
+  MyMuon muon(Muon_Px[i], Muon_Py[i], Muon_Pz[i], Muon_E[i]);
+  muon.SetIsolation(Muon_Iso[i]);
+  muon.SetCharge(Muon_Charge[i]);
+  Muons.push_back(muon);
+  }
 
-	Electrons.clear();
-	for (int i = 0; i < NElectron; ++i) {
-	MyElectron electron(Electron_Px[i], Electron_Py[i], Electron_Pz[i], Electron_E[i]);
-	electron.SetIsolation(Electron_Iso[i]);
-	electron.SetCharge(Electron_Charge[i]);
-	Electrons.push_back(electron);
-	}
+  Electrons.clear();
+  for (int i = 0; i < NElectron; ++i) {
+  MyElectron electron(Electron_Px[i], Electron_Py[i], Electron_Pz[i], Electron_E[i]);
+  electron.SetIsolation(Electron_Iso[i]);
+  electron.SetCharge(Electron_Charge[i]);
+  Electrons.push_back(electron);
+  }
 
-	Photons.clear();
-	for (int i = 0; i < NPhoton; ++i) {
-	MyPhoton photon(Photon_Px[i], Photon_Py[i], Photon_Pz[i], Photon_E[i]);
-	photon.SetIsolation(Photon_Iso[i]);
-	Photons.push_back(photon);
-	}
+  Photons.clear();
+  for (int i = 0; i < NPhoton; ++i) {
+  MyPhoton photon(Photon_Px[i], Photon_Py[i], Photon_Pz[i], Photon_E[i]);
+  photon.SetIsolation(Photon_Iso[i]);
+  Photons.push_back(photon);
+  }
 
-	Jets.clear();
-	for (int i = 0; i < NJet; ++i) {
-	MyJet jet(Jet_Px[i], Jet_Py[i], Jet_Pz[i], Jet_E[i]);
-	jet.SetBTagDiscriminator(Jet_btag[i]);
-	jet.SetJetID(Jet_ID[i]);
-	Jets.push_back(jet);
-	}
+  Jets.clear();
+  for (int i = 0; i < NJet; ++i) {
+  MyJet jet(Jet_Px[i], Jet_Py[i], Jet_Pz[i], Jet_E[i]);
+  jet.SetBTagDiscriminator(Jet_btag[i]);
+  jet.SetJetID(Jet_ID[i]);
+  Jets.push_back(jet);
+  }
 
-	hadB.SetXYZM(MChadronicBottom_px, MChadronicBottom_py, MChadronicBottom_pz, 4.8);
-	lepB.SetXYZM(MCleptonicBottom_px, MCleptonicBottom_py, MCleptonicBottom_pz, 4.8);
-	hadWq.SetXYZM(MChadronicWDecayQuark_px, MChadronicWDecayQuark_py, MChadronicWDecayQuark_pz, 0.0);
-	hadWqb.SetXYZM(MChadronicWDecayQuarkBar_px, MChadronicWDecayQuarkBar_py, MChadronicWDecayQuarkBar_pz, 0.0);
-	lepWl.SetXYZM(MClepton_px, MClepton_py, MClepton_pz, 0.0);
-	lepWn.SetXYZM(MCneutrino_px, MCneutrino_py, MCneutrino_pz, 0.0);
-	met.SetXYZM(MET_px, MET_py, 0., 0.);
+  hadB.SetXYZM(MChadronicBottom_px, MChadronicBottom_py, MChadronicBottom_pz, 4.8);
+  lepB.SetXYZM(MCleptonicBottom_px, MCleptonicBottom_py, MCleptonicBottom_pz, 4.8);
+  hadWq.SetXYZM(MChadronicWDecayQuark_px, MChadronicWDecayQuark_py, MChadronicWDecayQuark_pz, 0.0);
+  hadWqb.SetXYZM(MChadronicWDecayQuarkBar_px, MChadronicWDecayQuarkBar_py, MChadronicWDecayQuarkBar_pz, 0.0);
+  lepWl.SetXYZM(MClepton_px, MClepton_py, MClepton_pz, 0.0);
+  lepWn.SetXYZM(MCneutrino_px, MCneutrino_py, MCneutrino_pz, 0.0);
+  met.SetXYZM(MET_px, MET_py, 0., 0.);
 
-	EventWeight *= weight_factor;
+  EventWeight *= weight_factor;
    
 }
 
@@ -60,31 +60,31 @@ void MyAnalysis::Begin(TTree * /*tree*/) {
 
 void MyAnalysis::SlaveBegin(TTree * /*tree*/) {
    
-	TString option = GetOption();
+  TString option = GetOption();
 
-	h_Mmumu = new TH1F("Mmumu", "Invariant di-muon mass", 60, 60, 120);
-	h_Mmumu->SetXTitle("m_{#mu#mu}");
-	h_Mmumu->Sumw2();
-	histograms.push_back(h_Mmumu);
-	histograms_MC.push_back(h_Mmumu);
+  h_Mmumu = new TH1F("Mmumu", "Invariant di-muon mass", 60, 60, 120);
+  h_Mmumu->SetXTitle("m_{#mu#mu}");
+  h_Mmumu->Sumw2();
+  histograms.push_back(h_Mmumu);
+  histograms_MC.push_back(h_Mmumu);
 
-	h_NMuon = new TH1F("NMuon", "Number of muons", 7, 0, 7);
-	h_NMuon->SetXTitle("No. Muons");
-	h_NMuon->Sumw2();
-	histograms.push_back(h_NMuon);
-	histograms_MC.push_back(h_NMuon);
+  h_NMuon = new TH1F("NMuon", "Number of muons", 7, 0, 7);
+  h_NMuon->SetXTitle("No. Muons");
+  h_NMuon->Sumw2();
+  histograms.push_back(h_NMuon);
+  histograms_MC.push_back(h_NMuon);
 
-	h_Mee = new TH1F("Mee", "eletcron mass", 60, 60, 120);
-	h_Mee->SetXTitle("m_{ee}");
-	h_Mee->Sumw2();
-	histograms.push_back(h_Mee);
-	histograms_MC.push_back(h_Mee);
+  h_Mee = new TH1F("Mee", "eletcron mass", 60, 60, 120);
+  h_Mee->SetXTitle("m_{ee}");
+  h_Mee->Sumw2();
+  histograms.push_back(h_Mee);
+  histograms_MC.push_back(h_Mee);
 
-	h_NElectron = new TH1F("NElectron", "Number of Electron", 7, 0, 7);
-	h_NElectron->SetXTitle("No.Electron");
-	h_NElectron->Sumw2();
-	histograms.push_back(h_NElectron);
-	histograms_MC.push_back(h_NElectron);
+  h_NElectron = new TH1F("NElectron", "Number of Electron", 7, 0, 7);
+  h_NElectron->SetXTitle("No.Electron");
+  h_NElectron->Sumw2();
+  histograms.push_back(h_NElectron);
+  histograms_MC.push_back(h_NElectron);
 
   h_MuonEta = new TH1F("MuonEta", "Muon Eta", 64, 0, 3.2);
   h_MuonEta->SetXTitle("Muon Eta");
@@ -104,7 +104,7 @@ void MyAnalysis::SlaveBegin(TTree * /*tree*/) {
   histograms.push_back(h_metPt);
   histograms_MC.push_back(h_metPt);
 
-	for(int i=0 ; i < 9; i++){
+  for(int i=0 ; i < 9; i++){
 
   h_NJet_S[i] = new TH1F(Form("NJet_S%i_%s",i,option.Data()), "Number of jets", 12, 0, 12);
   h_NJet_S[i]->SetXTitle("Jet Multplicity");
@@ -129,103 +129,106 @@ void MyAnalysis::SlaveBegin(TTree * /*tree*/) {
 
 Bool_t MyAnalysis::Process(Long64_t entry) {
   
-   ++TotalEvents;
-   
-   GetEntry(entry);
-   
-   if (TotalEvents % 10000 == 0)
-      cout << "Next event -----> " << TotalEvents << endl;
-   
-   BuildEvent();
-   
-   double MuonPtCut = 30.;
-   double MuonRelIsoCut = 0.10;
-   double ElectronPtCut = 20;
-   double ElectronRelIsoCut = 0.3;
-   double JetPtCut = 35.;
-   double JetRelIsoCut = 0;
+  ++TotalEvents;
+
+  GetEntry(entry);
+
+  if (TotalEvents % 10000 == 0)
+    cout << "Next event -----> " << TotalEvents << endl;
+
+  BuildEvent();
+
+  double MuonPtCut = 30.;
+  double MuonRelIsoCut = 0.10;
+  double ElectronPtCut = 20;
+  double ElectronRelIsoCut = 0.3;
+  double JetPtCut = 35.;
+  double JetRelIsoCut = 0;
 
 /////Muon   
-	 int N_IsoMuon = 0;
-	 MyMuon *muon1, *muon2;
+  int N_IsoMuon = 0;
+  MyMuon *muon1, *muon2;
 
-	 for (vector<MyMuon>::iterator muon = Muons.begin(); muon != Muons.end(); ++muon) {
-		 if (muon->IsIsolated(MuonRelIsoCut) && abs(muon->Eta()) < 2.4) {
-			 ++N_IsoMuon;
-			 if (N_IsoMuon == 1) muon1 = &(*muon);
-			 if (N_IsoMuon == 2) muon2 = &(*muon);
-		 }
-	 }
-
-    if(N_IsoMuon == 1){
-      TLorentzVector Muon;
-      Muon.SetXYZM(Muon_Px[0],Muon_Py[0],Muon_Pz[0],0);
-       h_MuonEta->Fill(Muon.Eta(),EventWeight);
+  for (vector<MyMuon>::iterator muon = Muons.begin(); muon != Muons.end(); ++muon){
+    if (muon->IsIsolated(MuonRelIsoCut) && abs(muon->Eta()) < 2.4){
+      ++N_IsoMuon;
+      if (N_IsoMuon == 1) muon1 = &(*muon);
+      if (N_IsoMuon == 2) muon2 = &(*muon);
     }
+  }
 
-	 h_NMuon->Fill(N_IsoMuon, EventWeight);
+  if(N_IsoMuon == 1){
+    TLorentzVector Muon;
+    Muon.SetXYZM(Muon_Px[0],Muon_Py[0],Muon_Pz[0],0);
+    h_MuonEta->Fill(Muon.Eta(),EventWeight);
+  }
 
-	 if (N_IsoMuon > 1 && triggerIsoMu24) {
-		 if (muon1->Pt()>MuonPtCut) {
-			 h_Mmumu->Fill((*muon1 + *muon2).M(), EventWeight);
-		 }
-	 } 
+  h_NMuon->Fill(N_IsoMuon, EventWeight);
 
-////Electron
-	 int N_IsoElectron = 0 ;
-	 MyElectron *electron1, *electron2;
+  if (N_IsoMuon > 1 && triggerIsoMu24) {
+    if (muon1->Pt()>MuonPtCut) {
+      h_Mmumu->Fill((*muon1 + *muon2).M(), EventWeight);
+    }   
+  }  
 
-	 for (vector<MyElectron>::iterator elec = Electrons.begin(); elec != Electrons.end(); ++elec) {
-		 if (elec->IsIsolated(ElectronRelIsoCut) && abs(elec->Eta()) < 2.4) {
-			 ++N_IsoElectron;
-			 if (N_IsoElectron == 1) electron1 = &(*elec);
-			 if (N_IsoElectron == 2) electron2 = &(*elec);
-		 }
-	 }
+  ////Electron
+  int N_IsoElectron = 0 ;
+  MyElectron *electron1, *electron2;
 
-	 h_NElectron->Fill(N_IsoElectron, EventWeight);
-
-	 if (N_IsoElectron > 1 && triggerIsoMu24) {
-		 if (electron1->Pt()>ElectronPtCut) {
-			 h_Mee->Fill((*electron1 + *electron2).M(), EventWeight);
-		 }
-	 }
-
-////Jets
-	 MyJet *jet_1, *jet_2, *jet_3, *jet_4;
-	 MyJet *bjet_1, *bjet_2;
-
-		int N_Jets = 0;
-		int N_BJets = 0;
-		for (vector<MyJet>::iterator jt = Jets.begin(); jt != Jets.end(); ++jt) {
-			if (abs(jt->Eta()) < 2.4 && (jt->Pt()) > JetPtCut) ++N_Jets;
-			if (N_Jets == 1) jet_1 = &(*jt);
-      if (N_Jets == 2) jet_2 = &(*jt);
-      if (N_Jets == 3) jet_3 = &(*jt);
-      if (N_Jets == 4) jet_4 = &(*jt);
-
-			if(jt->IsBTagged()){ ++N_BJets;
-				if (N_BJets == 1) bjet_1 = &(*jt);
-				if (N_BJets == 2) bjet_2 = &(*jt);
-			}
-		}
-
-			h_NJet_S[0]->Fill(N_Jets, EventWeight);
-			h_NBJet_S[0]->Fill(N_BJets, EventWeight);
-
-			if (N_Jets > 1){
-      	if (jet_1->Pt()>JetPtCut){
-        	h_JetMass_S[0]->Fill((*jet_1 + *jet_2).M(),EventWeight);
-      	}
-			}
-
-    TLorentzVector Jets[N_Jets];
-    for(int i = 0; i < N_Jets; ++i){
-      Jets[i].SetXYZM(Jet_Px[i], Jet_Py[i], Jet_Pz[i], Jet_E[i]);
-      h_JetEta->Fill(Jets[i].Eta(),EventWeight);
+  for (vector<MyElectron>::iterator elec = Electrons.begin(); elec != Electrons.end(); ++elec) {
+    if (elec->IsIsolated(ElectronRelIsoCut) && abs(elec->Eta()) < 2.4) {
+      ++N_IsoElectron;
+      if (N_IsoElectron == 1) electron1 = &(*elec);
+      if (N_IsoElectron == 2) electron2 = &(*elec);
     }
+  }
 
-/////MET w/ 1 iso muon
+  h_NElectron->Fill(N_IsoElectron, EventWeight);
+
+  if (N_IsoElectron > 1 && triggerIsoMu24) {
+    if (electron1->Pt()>ElectronPtCut) {
+      h_Mee->Fill((*electron1 + *electron2).M(), EventWeight);
+    }
+  }
+
+  ////Jets
+  MyJet *jet_1, *jet_2, *jet_3, *jet_4;
+  MyJet *bjet_1, *bjet_2;
+
+  int N_Jets = 0;
+  int N_BJets = 0;
+
+  for (vector<MyJet>::iterator jt = Jets.begin(); jt != Jets.end(); ++jt) {
+    if (abs(jt->Eta()) < 2.4 && (jt->Pt()) > JetPtCut) ++N_Jets;
+
+    if (N_Jets == 1) jet_1 = &(*jt);
+    if (N_Jets == 2) jet_2 = &(*jt);
+    if (N_Jets == 3) jet_3 = &(*jt);
+    if (N_Jets == 4) jet_4 = &(*jt);
+
+    if(jt->IsBTagged()){ ++N_BJets;
+      if (N_BJets == 1) bjet_1 = &(*jt);
+      if (N_BJets == 2) bjet_2 = &(*jt);
+    }
+  }
+
+  h_NJet_S[0]->Fill(N_Jets, EventWeight);
+  h_NBJet_S[0]->Fill(N_BJets, EventWeight);
+
+  if (N_Jets > 1){
+    if (jet_1->Pt()>JetPtCut){
+      h_JetMass_S[0]->Fill((*jet_1 + *jet_2).M(),EventWeight);
+    }
+  }
+
+  TLorentzVector Jets[N_Jets];
+
+  for(int i = 0; i < N_Jets; ++i){
+    Jets[i].SetXYZM(Jet_Px[i], Jet_Py[i], Jet_Pz[i], Jet_E[i]);
+    h_JetEta->Fill(Jets[i].Eta(),EventWeight);
+  }
+
+  /////MET w/ 1 iso muon
   if(N_IsoMuon == 1){
     TLorentzVector MET;
     MET.SetXYZM(MET_px, MET_py, 0, 0);
@@ -234,30 +237,31 @@ Bool_t MyAnalysis::Process(Long64_t entry) {
 
 
 ////S1
-if (triggerIsoMu24){
+  if (triggerIsoMu24){
 
-      h_NJet_S[1]->Fill(N_Jets, EventWeight);
-      h_NBJet_S[1]->Fill(N_BJets, EventWeight);
+    h_NJet_S[1]->Fill(N_Jets, EventWeight);
+    h_NBJet_S[1]->Fill(N_BJets, EventWeight);
+
+    if (N_Jets > 1){
+      if (jet_1->Pt()>JetPtCut){
+        h_JetMass_S[1]->Fill((*jet_1 + *jet_2).M(),EventWeight);
+      }
+    }
+
+////S2
+
+    if(N_IsoMuon == 1){
+      h_NJet_S[2]->Fill(N_Jets, EventWeight);
+      h_NBJet_S[2]->Fill(N_BJets, EventWeight);
 
       if (N_Jets > 1){
         if (jet_1->Pt()>JetPtCut){
-          h_JetMass_S[1]->Fill((*jet_1 + *jet_2).M(),EventWeight);
+          h_JetMass_S[2]->Fill((*jet_1 + *jet_2).M(),EventWeight);
         }
       }
-////S2
- 
-	 	if(N_IsoMuon == 1){
-			h_NJet_S[2]->Fill(N_Jets, EventWeight);
-    	h_NBJet_S[2]->Fill(N_BJets, EventWeight);
+    }
 
-			if (N_Jets > 1){
-				if (jet_1->Pt()>JetPtCut){
-					h_JetMass_S[2]->Fill((*jet_1 + *jet_2).M(),EventWeight);
-				}
-			}
-		}
-
-////S3
+    ////S3
     if (N_Jets >= 4 && N_IsoMuon == 1) {
       h_NJet_S[3]->Fill(N_Jets, EventWeight);
       h_NBJet_S[3]->Fill(N_BJets, EventWeight);
@@ -266,10 +270,10 @@ if (triggerIsoMu24){
         if (jet_1->Pt()>JetPtCut){
           h_JetMass_S[3]->Fill((*jet_1 + *jet_2).M(),EventWeight);
         }
-      }
+      } 
     }
 
-////S4
+    ////S4
     if (N_IsoMuon == 1 &&  N_BJets >= 1) {
       h_NJet_S[4]->Fill(N_Jets, EventWeight);
       h_NBJet_S[4]->Fill(N_BJets, EventWeight);
@@ -281,31 +285,31 @@ if (triggerIsoMu24){
       }
     }
 
-////S5
-		if (N_Jets >= 4 && N_IsoMuon == 1 &&  N_BJets >= 1) {
-			h_NJet_S[5]->Fill(N_Jets, EventWeight);
-			h_NBJet_S[5]->Fill(N_BJets, EventWeight);
+    ////S5
+    if (N_Jets >= 4 && N_IsoMuon == 1 &&  N_BJets >= 1) {
+      h_NJet_S[5]->Fill(N_Jets, EventWeight);
+      h_NBJet_S[5]->Fill(N_BJets, EventWeight);
 
-			if (N_Jets >= 4){
-      	if (jet_1->Pt()>JetPtCut){
-        	h_JetMass_S[5]->Fill((*jet_1 + *jet_2).M(),EventWeight);
-      	}
-			}
-		}
+      if (N_Jets >= 4){
+        if (jet_1->Pt()>JetPtCut){
+          h_JetMass_S[5]->Fill((*jet_1 + *jet_2).M(),EventWeight);
+        }
+      }
+    }
 
-////S6
-		if (N_IsoMuon == 1 && N_BJets >= 2){
-			h_NJet_S[6]->Fill(N_Jets, EventWeight);
-   		h_NBJet_S[6]->Fill(N_BJets, EventWeight);
+    ////S6
+    if (N_IsoMuon == 1 && N_BJets >= 2){
+      h_NJet_S[6]->Fill(N_Jets, EventWeight);
+      h_NBJet_S[6]->Fill(N_BJets, EventWeight);
 
-			if (N_Jets > 1){
-       	if (jet_1->Pt()>JetPtCut){
-         	h_JetMass_S[6]->Fill((*jet_1 + *jet_2).M(),EventWeight);
-       	}
-     	}
-   	}
+      if (N_Jets > 1){
+        if (jet_1->Pt()>JetPtCut){
+          h_JetMass_S[6]->Fill((*jet_1 + *jet_2).M(),EventWeight);
+        }
+      }
+    }
 
-////S7
+    ////S7
     if (N_Jets >=4 && N_IsoMuon == 1 && N_BJets >= 2){
       h_NJet_S[7]->Fill(N_Jets, EventWeight);
       h_NBJet_S[7]->Fill(N_BJets, EventWeight);
@@ -317,22 +321,22 @@ if (triggerIsoMu24){
       }
     }
 
-///S8
-  	if(N_IsoMuon == 2){
-			h_NJet_S[8]->Fill(N_Jets, EventWeight);
-    	h_NBJet_S[8]->Fill(N_BJets, EventWeight);
+    ///S8
+    if(N_IsoMuon == 2){
+      h_NJet_S[8]->Fill(N_Jets, EventWeight);
+      h_NBJet_S[8]->Fill(N_BJets, EventWeight);
 
-    	if (N_Jets > 1){
-      	if (jet_1->Pt()>JetPtCut){
-        	h_JetMass_S[8]->Fill((*jet_1 + *jet_2).M(),EventWeight);
-      	}
-    	}
-		}
+      if (N_Jets > 1){
+        if (jet_1->Pt()>JetPtCut){
+          h_JetMass_S[8]->Fill((*jet_1 + *jet_2).M(),EventWeight);
+        }
+      }
+    }
 /////
-	}
+  }
 //////////////////////////////
-   
-   return kTRUE;
+  return kTRUE;
+
 }
 
 void MyAnalysis::SlaveTerminate() {
