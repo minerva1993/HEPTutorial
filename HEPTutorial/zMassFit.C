c1a6fbc4e1703246663ced52f5427ac1bdb45f90
Jiwon Park <minerva1993@gmail.com>
2017-09-07 14:54:39 +0900

    breit-wigner

--- HEPTutorial/zMassFit.C
+++ HEPTutorial/zMassFit.C
@@ -1,5 +1,6 @@
 #include <TFitResult.h>
 #include <TMath.h>
+Double_t mybw(Double_t* x, Double_t* par);
 
 void zMassFit(){
 
@@ -12,8 +13,8 @@ void zMassFit(){
 
   //create clone of hgg dist
   TH1F *h_mmumu = (TH1F*)h_Mmumu->Clone();
+  TH1F *h_mmumu2 = (TH1F*)h_Mmumu->Clone();
   TH1F *h_mmumumc = (TH1F*)h_Mmumumc->Clone();
-
 
   //define gaus fit fct
   TF1 *mygaus = new TF1("mygaus", "[0]/(sqrt(2.0*3.14151927)*[2])*exp(-(x-[1])*(x-[1])/(2.0*[2]*[2]))", 0.0, 200.0);
@@ -22,6 +23,13 @@ void zMassFit(){
     mygaus->SetParName(2, "Width");
     double initval1[3] = { 13000, 90.0, 1};
     mygaus->SetParameters(initval1);
+
+  TF1 *bw = new TF1("mybw", mybw, 0, 150, 3);
+    bw->SetParName(0, "Constant");
+    bw->SetParName(1, "Sigma");
+    bw->SetParName(2, "Mean");
+    double initval2[3] = { 1.0, 5.0, 90.0};
+    bw->SetParameters(initval2);
 
   //create canvas
   TCanvas *c=new TCanvas("c","DiMuon(Z) Mass",600,600);
@@ -36,6 +44,15 @@ void zMassFit(){
   TF1*Mmumu = new TF1("Mmumu", "[0]/(sqrt(2.0*3.14151927)*[2])*exp(-(x-[1])*(x-[1])/(2.0*[2]*[2]))", 30.0, 150.0);
   Mmumu->SetParameters(initval3);
 
+  TFitResultPtr r2= h_mmumu2->Fit(bw, "S");
+  Double_t par3   = r2->Parameter(0);
+  Double_t par4   = r2->Parameter(1);
+  Double_t par5   = r2->Parameter(2);
+  double initval4[3] = {par3,par4,par5};
+
+  TF1 *Mmumu2 = new TF1("Mmumu2", mybw, 0, 150, 3);
+  Mmumu2->SetParameters(initval4);
+
   //draw
   c->cd();
   h_mmumu->Draw("P");
@@ -45,6 +62,8 @@ void zMassFit(){
   h_mmumu->GetXaxis()->SetRangeUser(0,400);
   h_mmumu->SetTitle("DiMuon Mass;Mass (GeV);Events");
   h_mmumu->GetYaxis()->SetTitleOffset(1.5);
+  h_mmumu2->Draw("p same");
+  h_mmumu2->SetMarkerSize(0.);
   h_mmumumc->Draw("hist same");
   h_mmumumc->SetFillStyle(0);
   h_mmumumc->SetLineColor(3);
@@ -54,4 +73,11 @@ void zMassFit(){
 
   c->Print("fitZ.pdf");
 }
-     
+
+Double_t mybw(Double_t* x, Double_t* par){
+  Double_t arg1 = 14.0/22.0; // 2 over pi
+  Double_t arg2 = par[1]*par[1]*par[2]*par[2]; //Gamma=par[1]  M=par[2]
+  Double_t arg3 = ((x[0]*x[0]) - (par[2]*par[2]))*((x[0]*x[0]) - (par[2]*par[2]));
+  Double_t arg4 = x[0]*x[0]*x[0]*x[0]*((par[1]*par[1])/(par[2]*par[2]));
+  return par[0]*arg1*arg2/(arg3 + arg4);
+}  
